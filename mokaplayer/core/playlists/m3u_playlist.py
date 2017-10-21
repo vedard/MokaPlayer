da4cab1855cae137faeb3ebca32939a1b965ee69
Vincent Bédard <vedard@gmail.com>
2017-10-21 15:26:55 -0400

    Support for relative path in m3u playlist

--- mokaplayer/core/playlists/m3u_playlist.py
+++ mokaplayer/core/playlists/m3u_playlist.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from mokaplayer.core.playlists import AbstractPlaylist
 from mokaplayer.core.database import Song
 
@@ -11,12 +13,19 @@ class M3UPlaylist(AbstractPlaylist):
         return self.m3u_parser.name
 
     def songs(self, order=AbstractPlaylist.OrderBy.DEFAULT, desc=False):
+        query = Song.select().where(Song.Path << list(self.m3u_parser) |
+                                    Song.Path << [pathlib.Path(self.m3u_parser.location).parent / (x)
+                                                  for x in self.m3u_parser])
+
         if order == self.OrderBy.DEFAULT:
-            result = {x.Path: x for x in Song.select()}
+            result = {x.Path: x for x in query}
             for path in self.m3u_parser:
-                yield result[path]
+                if path in result:
+                    yield result[path]
+                else:
+                    path_abs = str(pathlib.Path(self.m3u_parser.location).parent / (path))
+                    if path_abs in result:
+                        yield result[path_abs]
         else:
-            for song in (Song.select()
-                         .where(Song.Path << list(self.m3u_parser))
-                         .order_by(*self.get_orderby_fields(order, desc))):
+            for song in (query.order_by(*self.get_orderby_fields(order, desc))):
                 yield song
