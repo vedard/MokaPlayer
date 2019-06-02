5b78ef6d344f1a3e2c86d2435beeab05da3bc7f8
= <vedard@gmail.com>
2019-06-02 18:14:20 -0400

    Fix problem while loading multiple image in a thread

--- mokaplayer/ui/gtk/helper/image_helper.py
+++ mokaplayer/ui/gtk/helper/image_helper.py
@@ -14,13 +14,13 @@ def load(filename, width, height, screen_scale_factor):
 
     pixbuf = GdkPixbuf.Pixbuf.new_from_file(filename)
     pixbuf = pixbuf.scale_simple(width * screen_scale_factor, height * screen_scale_factor, GdkPixbuf.InterpType.BILINEAR)
-    return Gdk.cairo_surface_create_from_pixbuf(pixbuf, screen_scale_factor)
+    return pixbuf
 
 
 def set_image(gtkimage, filename, width, height):
     screen_scale_factor = gtkimage.get_scale_factor()
-    surface = load(filename, width, height, screen_scale_factor)
-    GObject.idle_add(lambda: gtkimage.set_from_surface(surface))
+    pixbuf = load(filename, width, height, screen_scale_factor)
+    GObject.idle_add(lambda: gtkimage.set_from_surface(Gdk.cairo_surface_create_from_pixbuf(pixbuf, screen_scale_factor)))
 
 
 def set_multiple_image(list_tuples):
